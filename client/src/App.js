3dc9fa715f75c25b017da8250b4f4b83c5bc8d8a
Justin Sue <justincsue@gmail.com>
2020-02-21 19:44:25 -0800

    savey save

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,24 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Login from "./components/Login";
+import SplashPage from "./components/SplashPage.js";
+import PrivateRoute from "./PrivateRoute";
+import BubblePage from "./components/BubblePage"
 import "./styles.scss";
 
 function App() {
+  const [colorList, setColorList] = useState([]);
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
+      <Route exact path="/" component={SplashPage} />
+        <Route exact path="/login" component={Login} />
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
-        */}
+        */
+     <PrivateRoute path='/bubblepage' component={BubblePage} />
+       }
       </div>
     </Router>
   );
