f73761656fdab55d0468b1d919ad46746bee3f50
Justin Sue <justincsue@gmail.com>
2020-02-21 21:10:25 -0800

    almost finished

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Login from "./components/Login";
@@ -8,7 +8,7 @@ import BubblePage from "./components/BubblePage"
 import "./styles.scss";
 
 function App() {
-  const [colorList, setColorList] = useState([]);
+  // const [colorList, setColorList] = useState([]);
   return (
     <Router>
       <div className="App">
